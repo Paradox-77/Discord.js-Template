1158fa0022a64aa0042b2f91db59eba775ccf935
Paradox-77 <66990101+Paradox-77@users.noreply.github.com>
2021-05-01 19:01:50 +0530

    Added a database to the client.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const Discord = require('discord.js');
+const db = require('quick.db')
 const client = new Discord.Client();
 const config = require('./config.json')
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 client.config = config
+client.db = db
 
 var handlers = ["command_handler", "event_handler"];
 handlers.forEach(handler => {
